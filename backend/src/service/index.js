a216e60a15158e9bb2f853be34b69050266ae43c
Carmen1796 <34404682+Carmen1796@users.noreply.github.com>
2017-12-14 12:33:49 +0200

    Update index.js

--- backend/src/service/index.js
+++ backend/src/service/index.js
@@ -1,4 +1,9 @@
-const user = require('./user');
+const client = require('./client');
+const medicament = require('./medicament');
+const reteta = require('./reteta');
+const vanzare=require('./vanzare');
+const bon = require('./bon');
+
 
 module.exports = {
   user,
